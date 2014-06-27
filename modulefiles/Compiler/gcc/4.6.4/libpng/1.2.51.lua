489f68be27b305dd4745336f2efe2e599b3a3b86
Axel <axel.huebl@web.de>
2014-06-27 18:55:25 +0200

    Use CMAKE_PREFIX_PATH to find libpng

--- modulefiles/Compiler/gcc/4.6.4/libpng/1.2.51.lua
+++ modulefiles/Compiler/gcc/4.6.4/libpng/1.2.51.lua
@@ -9,6 +9,7 @@ local version = "1.2.51"
 local base = pathJoin("/opt/gcc/4.6.4-software/libpng",version)
 
 setenv( "PNG_ROOT", base)
+prepend_path("CMAKE_PREFIX_PATH", base)
 prepend_path("MANPATH", pathJoin(base, "share/man"))
 prepend_path("PATH", pathJoin(base, "bin"))
 prepend_path("C_INCLUDE_PATH", pathJoin(base, "include"))
