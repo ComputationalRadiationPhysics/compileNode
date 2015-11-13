aed81bf52806886218d144dfca634f7935d4666f
ax3l <axel.huebl@web.de>
2015-11-13 12:45:19 +0100

    New Modules & Updated Installs

---
+++ modulefiles/Compiler/gcc/4.6.4/pngwriter/0.5.5.lua
@@ -0,0 +1,21 @@
+help(
+[[
+This module loads PNGwriter
+]])
+
+whatis("Description: PNGwriter: C++ library for creating PNG images")
+
+local version = "0.5.5"
+local base = pathJoin("/opt/gcc/4.6.4-software/pngwriter",version)
+
+-- load libpng
+if (mode() == "load") then
+  if ( not isloaded("libpng") ) then
+    load("libpng")
+  end
+end
+
+setenv( "PNGWRITER_ROOT", base)
+-- prepend_path("C_INCLUDE_PATH", pathJoin(base, "include"))
+-- prepend_path("CPLUS_INCLUDE_PATH", pathJoin(base, "include"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
---
+++ modulefiles/MPI/gcc/4.6.4/openmpi/1.6.5/adios/1.8.0.lua
@@ -0,0 +1,25 @@
+help(
+[[
+This module loads ADIOS.
+]])
+
+whatis("Description: ADIOS: The Adaptable IO System")
+
+local version = "1.8.0"
+local base = pathJoin("/opt/gcc/4.6.4-software/openmpi/1.6.5-software/adios",version)
+
+-- prereq("hdf5/1.8.13")
+
+-- load Mini-XML
+if (mode() == "load") then
+  if ( not isloaded("mxml") ) then
+    load("mxml")
+  end
+end
+
+setenv("ADIOS_ROOT", base)
+prepend_path("PATH", pathJoin(base, "bin"))
+-- prepend_path("C_INCLUDE_PATH", pathJoin(base, "include"))
+-- prepend_path("CPLUS_INCLUDE_PATH", pathJoin(base, "include"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+-- prepend_path("PYTHONPATH", pathJoin(base, "bin"))
