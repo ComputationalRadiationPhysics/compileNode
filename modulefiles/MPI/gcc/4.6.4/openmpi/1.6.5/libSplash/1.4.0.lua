a39d3e7c290e3882bc97568c769e6753241cf16d
Axel Huebl <axel.huebl@plasma.ninja>
2016-04-13 06:42:06 +0200

    libSplash 1.4.0 Release
    Add to compile chains

---
+++ modulefiles/MPI/gcc/4.6.4/openmpi/1.6.5/libSplash/1.4.0.lua
@@ -0,0 +1,27 @@
+help(
+[[
+This module loads libSplash.
+The parallel mode is enabled.
+]])
+
+whatis("Description: libSplash: Simple Parallel file output Library for Accumulating Simulation data using Hdf5")
+
+local version = "1.4.0"
+local base = pathJoin("/opt/gcc/4.6.4-software/openmpi/1.6.5-software/libSplash",version)
+
+-- prereq("hdf5/1.8.13")
+-- prereq("openmpi/1.6.5")
+
+-- load HDF5
+if (mode() == "load") then
+  if ( not isloaded("hdf5") ) then
+    load("hdf5")
+  end
+end
+
+setenv( "SPLASH_ROOT", base)
+prepend_path("PATH", pathJoin(base, "bin"))
+-- prepend_path("C_INCLUDE_PATH", pathJoin(base, "include"))
+-- prepend_path("CPLUS_INCLUDE_PATH", pathJoin(base, "include"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+prepend_path("PYTHONPATH", pathJoin(base, "bin"))
